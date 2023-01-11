490115ccb3be0c79398802a20a2d24b87ccc90bf
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-11 20:42:38 +0100

    Reading from dotfile data

--- muscli.py
+++ muscli.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
+path = '/home/' + os.environ.get('USER') + '/.muscli/data'
+
+with open(path, 'r') as file:
+    lines = file.readlines()
+
+if len(sys.argv) == 1:
+    last_number = lines[1]
+    last_day = lines[2]
