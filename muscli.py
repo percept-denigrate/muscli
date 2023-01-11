3f09d757981048a7b8680781915278ef99bea258
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-11 21:38:45 +0100

    Updates data file

--- muscli.py
+++ muscli.py
@@ -6,11 +6,14 @@ import datetime as dt
 
 path = '/home/' + os.environ.get('USER') + '/.muscli/data'
 
-with open(path, 'r') as file:
-    lines = file.readlines()
-
 if len(sys.argv) == 1:
-    last_number = lines[1][0:-1]
+    with open(path, 'r') as file:
+        lines = file.readlines()
+    last_number = int(lines[1][0:-1])
     last_day = lines[2][0:-1]
     diff = dt.date.today()-dt.datetime.strptime(last_day,'%Y/%m/%d').date()
     d = diff.days
+    print(d)
+    data_new = [lines[0], str((last_number+d) % int(lines[0][0:-1]))+'\n', dt.date.today().strftime('%Y/%m/%d')]
+    with open(path, 'w') as file:
+        file.writelines(data_new)
