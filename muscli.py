a5fade3823e6209ef7be068e27f5e8b504189bcb
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-11 22:16:14 +0100

    Writes data and prints

--- muscli.py
+++ muscli.py
@@ -4,16 +4,24 @@ import os
 import sys
 import datetime as dt
 
-path = '/home/' + os.environ.get('USER') + '/.muscli/data'
+path = '/home/' + os.environ.get('USER') + '/.muscli/'
 
 if len(sys.argv) == 1:
-    with open(path, 'r') as file:
+    with open(path + 'data', 'r') as file:
         lines = file.readlines()
     last_number = int(lines[1][0:-1])
-    last_day = lines[2][0:-1]
-    diff = dt.date.today()-dt.datetime.strptime(last_day,'%Y/%m/%d').date()
+    last_day = lines[2]
+    print(last_day)
+    last_day_dt = dt.datetime.strptime(last_day,'%Y/%m/%d')
+    print(last_day_dt)
+    diff = dt.date.today()-last_day_dt.date()
+    print(dt.date.today(),dt.datetime.strptime(last_day,'%Y/%m/%d').date())
     d = diff.days
     print(d)
-    data_new = [lines[0], str((last_number+d) % int(lines[0][0:-1]))+'\n', dt.date.today().strftime('%Y/%m/%d')]
-    with open(path, 'w') as file:
+    new_number = str((last_number+d) % int(lines[0][0:-1]))
+    data_new = [lines[0], new_number +'\n', dt.date.today().strftime('%Y/%m/%d')]
+    with open(path + 'data', 'w') as file:
         file.writelines(data_new)
+
+    with open(path + new_number, 'r') as file:
+        print(file.read())
