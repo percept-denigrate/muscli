8dd208027d00fa11c7170cbd86a0ffba074c96de
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-11 21:09:52 +0100

    Get time delta from last use

--- muscli.py
+++ muscli.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import datetime as dt
 
 path = '/home/' + os.environ.get('USER') + '/.muscli/data'
 
@@ -9,5 +10,7 @@ with open(path, 'r') as file:
     lines = file.readlines()
 
 if len(sys.argv) == 1:
-    last_number = lines[1]
-    last_day = lines[2]
+    last_number = lines[1][0:-1]
+    last_day = lines[2][0:-1]
+    diff = dt.date.today()-dt.datetime.strptime(last_day,'%Y/%m/%d').date()
+    d = diff.days
