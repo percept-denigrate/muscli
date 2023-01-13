3f525a6ffc8bfc90f0986996058b85a2db638a43
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-13 14:50:42 +0100

    Increment feature finished

--- muscli.py
+++ muscli.py
@@ -30,7 +30,16 @@ elif len(sys.argv) == 4 and sys.argv[1] == 'incr':
     for i in range(n): #search in each workout
         with open(path + str(i), 'r') as file: 
             workout = file.readlines()
-        for exercise in workout: #check each line
-            words = exercise[0:-1].split(' ')
+        for j in range(len(workout)): #check each line
+            words = workout[j][0:-1].split(' ')
             if words[0] == sys.argv[2]: #when exercise is found
-                print(words[0])
+                workout_new = workout[:]
+                exercise_new = '' #recreates the line with the new weight
+                for k in range(len(words)-1):
+                    exercise_new += words[k] + ' '
+                exercise_new += str(float(words[-1]) + float(sys.argv[3]))
+                exercise_new += '\n'
+                print(exercise_new)
+                workout_new[j] = exercise_new
+                with open(path + str(i), 'w') as file: #updates workout file
+                    file.writelines(workout_new)
