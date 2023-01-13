dccbf8815e6cc5413d0e60eb3034478f8e53b28f
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-01-13 15:56:32 +0100

    Data read factorization

--- muscli.py
+++ muscli.py
@@ -7,26 +7,23 @@ import datetime as dt
 path = '/home/' + os.environ.get('USER') + '/.muscli/'
 date_format = '%Y/%m/%d'
 
-if len(sys.argv) == 1:
-    with open(path + 'data', 'r') as file:    #reads data file
-        lines = file.readlines()
-    last_number = int(lines[1][0:-1])
-    last_day = lines[2][0:-1]
+with open(path + 'data', 'r') as file:    #reads data file
+    lines = file.readlines()
+n = int(lines[0][0:-1])
+day = int(lines[1][0:-1])
+last_called = lines[2][0:-1]
 
-    diff = dt.date.today()-dt.datetime.strptime(last_day,date_format).date()    #number of days since last use
-    new_number = str((last_number+diff.days) % int(lines[0][0:-1]))
-    data_new = [lines[0], new_number +'\n', dt.date.today().strftime(date_format) + '\n']
+if len(sys.argv) == 1:
+    diff = dt.date.today()-dt.datetime.strptime(last_called,date_format).date()    #number of days since last use
+    day_new = str((day+diff.days) % int(lines[0][0:-1]))
+    data_new = [lines[0], day_new +'\n', dt.date.today().strftime(date_format) + '\n']
     with open(path + 'data', 'w') as file:    #updates data file
         file.writelines(data_new)
 
-    with open(path + new_number, 'r') as file:    #prints workout
+    with open(path + day_new, 'r') as file:    #prints workout
         print(file.read())
 
 elif len(sys.argv) == 4 and sys.argv[1] == 'incr':
-    with open(path + 'data', 'r') as file:    #reads data file
-        lines = file.readlines()
-    n = int(lines[0][0:-1])
-
     for i in range(n):    #search in each workout
         with open(path + str(i), 'r') as file: 
             workout = file.readlines()
@@ -45,10 +42,7 @@ elif len(sys.argv) == 4 and sys.argv[1] == 'incr':
                     file.writelines(workout_new)
 
 elif len(sys.argv) == 2 and sys.argv[1] == 'shift':
-    with open(path + 'data', 'r') as file:    #reads data file
-        lines = file.readlines()
-    day = lines[1][0:-1]
     data_new = lines[:]
-    data_new[1] = str(int(day)-1) + '\n'
+    data_new[1] = str(day-1) + '\n'
     with open(path + 'data', 'w') as file:    #updates data file
         file.writelines(data_new)
